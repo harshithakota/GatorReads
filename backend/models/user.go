315d1e2b5e1718042c33a0d32cb44d062dd2e7c2
AnuguAdithi <anuguadithi4040@gmail.com>
2025-02-08 21:18:01 -0500

    change user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,16 +4,14 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	UserType     string `json:"userType"`
-	UserFullName string `json:"userFullName"`
-	AdmissionID  string `json:"admissionId,omitempty"`
-	EmployeeID   string `json:"employeeId,omitempty"`
-	Age          int    `json:"age"`
-	DOB          string `json:"dob"`
-	Gender       string `json:"gender"`
-	Address      string `json:"address"`
-	MobileNumber string `json:"mobileNumber"`
-	Email        string `json:"email" gorm:"unique"`
-	Password     string `json:"password"`
-	IsAdmin      bool   `json:"isAdmin"`
+	UserType     string json:"userType"
+	UserFullName string json:"userFullName"
+	UFID         string json:"ufid" gorm:"column:ufid" // Changed to uppercase
+
+	DOB    string json:"dob"
+	Gender string json:"gender"
+
+	Email    string json:"email" gorm:"unique"
+	Password string json:"password"
+	IsAdmin  bool   json:"isAdmin"
 }
