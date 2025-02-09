7f2f5d18db641709c89f6caa06fc42864e1dd285
harshithakota <your.harshithachowdarykota@gmail.com>
2025-02-08 19:57:19 -0500

    Add stats in homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import About from '../components/About'
 import WelcomeBox from '../components/WelcomeBox'
+import Stats from '../components/Stats'
 
 function Home() {
     return (
         <div id='home'>
             <WelcomeBox/>
             <About/>
+            <Stats/>
         </div>
     )
 }
