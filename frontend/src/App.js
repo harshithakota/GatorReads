0a72e0af93a0530c00781cd99427341efb2759e8
KrishnaVbs1001 <vankayalakrishna1001@gmail.com>
2025-02-09 17:44:26 -0500

    Modified front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,30 @@ import Register from './components/auth/Register';
 import Home from './components/Home';
 import Header from './components/Header';
 import StudentDashboard from './components/StudentDashboard';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0021A5', // UF Blue
+      light: '#6C9AC3',
+      dark: '#001433',
+    },
+    secondary: {
+      main: '#FA4616', // UF Orange
+      light: '#FF8E6C',
+      dark: '#C83812',
+    },
+    background: {
+      default: '#E5F0FF',
+      paper: '#FFFFFF',
+    },
+    text: {
+      primary: '#001433',
+      secondary: '#0021A5',
+    },
+  },
+});
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
