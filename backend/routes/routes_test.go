49548cf670405a7ec2b0af7570e9bc9d06224d4e
AnuguAdithi <anuguadithi4040@gmail.com>
2025-03-02 19:09:08 -0500

    added DB details for connecting to test database

--- backend/routes/routes_test.go
+++ backend/routes/routes_test.go
@@ -1,7 +1,15 @@
 package routes
 
 import (
+	"backend/database"
+	"backend/models"
+	"fmt"
+	"os"
+	"testing"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func setupRouter() *gin.Engine {
@@ -17,4 +25,24 @@ func setupRouter() *gin.Engine {
 	router.POST("/addBook", AddBook)
 
 	return router
+}
+
+func TestMain(m *testing.M) {
+	// Set up database connection
+	//dsn := "host=localhost user=your_user password=your_password dbname=gatorreads_test port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=adithi password='adithi_anugu' dbname=gatorreads_test port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		panic(err)
+	}
+	database.DB = db
+
+	// Optionally, auto-migrate your schemas here
+	database.DB.AutoMigrate(&models.Book{}, &models.User{})
+
+	exitVal := m.Run()
+
+	os.Exit(exitVal)
 }
